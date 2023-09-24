806693130ad416f843e1da8b1cbdcaaf2c78d060
Louis Aussedat <aussedat.louis@gmail.com>
2023-09-24 22:47:49 +0200

    [code] improve CustomeNode typing

--- src/CustomNode.tsx
+++ src/CustomNode.tsx
@@ -1,10 +1,21 @@
-import { useCallback } from 'react'
 import { Handle, Position } from 'reactflow'
 
-function CustomNode({ data, isConnectable }) {
-  const onChange = useCallback((evt) => {
-    console.log(evt.target.value)
-  }, [])
+type NodeData = {
+  image: string
+  title: string
+  artist: string
+  key: string
+  bpm: string
+  style: string
+}
+
+type Props = {
+  data: NodeData
+  isConnectable?: boolean
+}
+
+const CustomNode = ({ data, isConnectable }: Props) => {
+  const { image, title, artist, key, bpm, style } = data
 
   return (
     <div className="text-updater-node shadow-md">
@@ -16,11 +27,14 @@ function CustomNode({ data, isConnectable }) {
         className="h-3 w-3 bg-purple"
       />
       <div>
-        <img src={data.image} style={{ width: '20%'}} /> <span>{data.title} - {data.artist}</span>
-        <hr className='h-px my-2 bg-light-purple border-0'/>
-        <span>Key: {data.key}</span>
-        <span>BPM: {data.bpm}</span>
-        <span>Style: {data.style}</span>
+        <img src={image} style={{ width: '20%' }} />{' '}
+        <span>
+          {title} - {artist}
+        </span>
+        <hr className="bg-light-purple my-2 h-px border-0" />
+        <span>Key: {key}</span>
+        <span>BPM: {bpm}</span>
+        <span>Style: {style}</span>
       </div>
       <Handle
         type="source"
