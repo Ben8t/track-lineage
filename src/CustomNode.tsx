e5bdfb9f30baa66cec084469dd29c7fa99ffd0d1
Benoit Pimpaud <pimpaudben@gmail.com>
2023-09-24 11:50:31 +0200

    [style] add style with custom node and update color

---
+++ src/CustomNode.tsx
@@ -0,0 +1,25 @@
+import { useCallback } from 'react';
+import { Handle, Position } from 'reactflow';
+
+
+function CustomNode({ data, isConnectable }) {
+  const onChange = useCallback((evt) => {
+    console.log(evt.target.value);
+  }, []);
+
+  return (
+    <div className="text-updater-node shadow-md">
+      <Handle type="target" position={Position.Left} id="target" isConnectable={isConnectable} className="h-3 w-3 bg-purple"/>
+      <div>
+        <label>{data.label}</label>
+        <hr />
+        <span>Key: {data.key}</span>
+        <span>BPM: {data.bpm}</span>
+        <span>Style: {data.style}</span>
+      </div>
+      <Handle type="source" position={Position.Right} id="source" isConnectable={isConnectable} className="h-3 w-3 bg-purple" />
+    </div>
+  );
+}
+
+export default CustomNode;
--- src/SearchList.tsx
+++ src/SearchList.tsx
@@ -15,7 +15,8 @@ function SearchList({ nodes, setNodes }: Props) {
     setNodes(
       nodes.concat({
         id: node.id,
-        data: { label: node.data.label },
+        type: "customNode",
+        data: { label: node.data.label, bpm: node.data.bpm, key: node.data.key, style: node.data.style },
         position: { x: 500, y: 25 },
       }),
     )
@@ -27,12 +28,12 @@ function SearchList({ nodes, setNodes }: Props) {
         <div className="max-w-sm p-6 m-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <form className="form grid grid-cols-2" onSubmit={(e) => onSubmit(node, e)} key={node.id}>
             <div>
-              <p className="font-mono font-normal text-test dark:text-gray-400" key={node.data.label}>
+              <p className="font-mono font-normal text-purple dark:text-gray-400" key={node.data.label}>
                 {node.data.label}
               </p>
             </div>
             <div>
-              <button className="font-mono col-span-1 bg-test hover:bg-test-700 text-white font-bold py-2 px-4 rounded" type="submit">
+              <button className="font-mono col-span-1 bg-purple hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" type="submit">
                 Add Track
               </button>
             </div>
