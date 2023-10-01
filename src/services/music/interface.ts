91124f49a31e6469767a22957a21219f01b55265
Louis Aussedat <aussedat.louis@gmail.com>
2023-10-01 14:00:23 +0200

    [code] use arrow function syntax instead of function keyword
    and run prettier

--- src/services/music/interface.ts
+++ src/services/music/interface.ts
@@ -20,24 +20,24 @@ export interface Track {
 }
 
 export interface AudioFeatures {
-  acousticness: number,
-  analysis_url: string,
-  danceability: number,
-  duration_ms: number,
-  energy: number,
-  id: string,
-  instrumentalness: number,
-  key: number,
-  liveness: number,
-  loudness: number,
-  mode: number,
-  speechiness: number,
-  tempo: number,
-  time_signature: number,
-  track_href: string,
-  type: string,
-  uri: string,
-  valence:number
+  acousticness: number
+  analysis_url: string
+  danceability: number
+  duration_ms: number
+  energy: number
+  id: string
+  instrumentalness: number
+  key: number
+  liveness: number
+  loudness: number
+  mode: number
+  speechiness: number
+  tempo: number
+  time_signature: number
+  track_href: string
+  type: string
+  uri: string
+  valence: number
 }
 
 export interface Album {
