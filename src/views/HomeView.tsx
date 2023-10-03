9922027947b3388e1be5ee12fe81b73c60a590e0
Benoit Pimpaud <pimpaudben@gmail.com>
2023-10-03 22:39:48 +0200

    [feature] create two routes

---
+++ src/views/HomeView.tsx
@@ -0,0 +1,21 @@
+import 'reactflow/dist/style.css'
+import '../custom_node.css'
+import Header from '../Header'
+import Flow from '../Flow'
+import Search from '../Search'
+
+
+const HomeView = () => {
+  return (
+    <>
+      <Header />
+
+      <div className="m-2 grid grid-cols-4 gap-2">
+        <Flow />
+        <Search />
+      </div>
+    </>
+  )
+}
+
+export default HomeView
---
+++ src/views/TestView.tsx
@@ -0,0 +1,13 @@
+import 'reactflow/dist/style.css'
+import '../custom_node.css'
+
+
+const TestView = () => {
+  return (
+    <>
+    Hello world
+    </>
+  )
+}
+
+export default TestView
